ea6bd459301023aaac4d6071f41128908b590899
Descalzo1980 <ragioniere1980@gmail.com>
2022-08-16 21:44:06 +0300

    add adapter observer recycle list

--- app/src/main/java/com/example/studentregister/MainActivity.kt
+++ app/src/main/java/com/example/studentregister/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.studentregister.db.Student
 import com.example.studentregister.db.StudentDatabase
 
@@ -15,7 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var saveButton: Button
     private lateinit var clearButton: Button
 
+
     private lateinit var viewModel: StudentViewModel
+    private lateinit var studentRecyclerView: RecyclerView
+    private lateinit var adapter: StudentRecycleViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         emailEditText = findViewById(R.id.et_email)
         saveButton = findViewById(R.id.btn_save)
         clearButton = findViewById(R.id.btn_clear)
+        studentRecyclerView = findViewById(R.id.rv_students)
 
         val dao = StudentDatabase.getInstance(application).studentDao()
         val factory = StudentViewModelFactory(dao)
@@ -38,6 +44,8 @@ class MainActivity : AppCompatActivity() {
         clearButton.setOnClickListener{
             clearInput()
         }
+
+        initRecycleView()
     }
 
     private fun saveStudentData(){
@@ -58,5 +66,19 @@ class MainActivity : AppCompatActivity() {
     private fun clearInput(){
         nameEditText.setText("")
         emailEditText.setText("")
+    }
+
+    private fun initRecycleView(){
+        studentRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = StudentRecycleViewAdapter()
+        studentRecyclerView.adapter = adapter
+        displayStudentList()
+    }
+
+    private fun displayStudentList(){
+        viewModel.students.observe(this,{
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
+        })
     }
 }
